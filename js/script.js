4aa33c7d1610c596c4f80ca3ca7f5991fa10c021
abusayadebu <abusayad743@gmail.com>
2026-03-10 17:55:40 +0600

    loded words from the api by clicking dynamic buttons and showed the words in the cards with active button color

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
 // catch the levelBtnSection
 const levelBtnSection = document.getElementById("levelBtnSection")
+const wordsSection = document.getElementById("wordsSection")
+const noLessonSection = document.getElementById("noLessonSection")
+
+
+// click levelbutton function
+function clickLevelBtn(id, btn){
+     // hide the noLessonSection 
+    noLessonSection.classList.add("hidden")
+    // show word section
+    wordsSection.classList.remove("hidden")
+
+    // remove active style from all buttons
+    const allBtns = levelBtnSection.querySelectorAll("button")
+    allBtns.forEach(b => {
+        b.classList.add("btn-outline")
+    })
+
+    // set active button
+    btn.classList.remove("btn-outline")
+    btn.classList.add("btn-primary")
+
+    loadWordsByLevels(id)
+}
 
 // load the level button api
 async function loadLevelBtns (){
@@ -8,16 +31,53 @@ async function loadLevelBtns (){
     displayLevelBtns(data.data);
 }
 
-
 // load the level button api
 async function displayLevelBtns (levels){
     levels.forEach(level => {
         const btn = document.createElement("button")
-        btn.innerHTML = ` <button class="btn btn-outline btn-primary"><span><i class="fa-solid fa-book-open"></i></span>Lesson: ${level.level_no}</button>`
+        btn.classList.add("btn","btn-outline","btn-primary")
+        btn.innerHTML = `<span><i class="fa-solid fa-book-open"></i></span> Lesson: ${level.level_no}`
 
-        // append to the levelBtnSection
+        btn.onclick = () => clickLevelBtn(level.level_no, btn)
+
+        // append
         levelBtnSection.appendChild(btn)
     });
 }
+
+// loadWordsByLevels
+async function loadWordsByLevels (id){
+    const res = await fetch(`https://openapi.programming-hero.com/api/level/${id}`)
+    const data = await res.json()
+    displayWordsByLevels(data.data);
+}
+
+// displayWordsByLevels
+async function displayWordsByLevels (words){
+    // clear
+    wordsSection.innerHTML = "";
+    words.forEach(word =>{
+        const card = document.createElement("div")
+        card.innerHTML = `<div class="card bg-base-100 shadow-sm">
+  <div class="card-body items-center text-center">
+    <h2 class="card-title text-2xl font-bold">${word.word}</h2>
+    <p class="text-lg font-medium my-5">meaning/pronunciation</span></p>
+    <h4 class="font-hind-siliguri font-semibold text-2xl text-[#575765]">${word.meaning} <span>/ ${word.pronunciation}</span></h4>
+    <div class="flex justify-between w-full mt-14">
+      <button class="btn btn-sm bg-[#00bbff2c] text-lg"><i class="fa-solid fa-circle-info"></i></button>
+      <button class="btn btn-sm bg-[#00bbff2c] text-lg"><i class="fa-solid fa-volume-high"></i></button>
+    </div>
+  </div>
+</div>
+        `
+
+        wordsSection.appendChild(card)
+    })
+}
+
+
+
+
+
 
 loadLevelBtns()
